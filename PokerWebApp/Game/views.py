c6db1556021d8ec207119fac13847cbfb30f2011
Aakaash Kapoor <aakkapoor@ucdavis.edu>
2022-01-14 01:37:19 -0800

    Learned how to use static files and context. Added poker table skeleton.

--- PokerWebApp/Game/views.py
+++ PokerWebApp/Game/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = sampleHandForAllPlayers()
+    return render(request, 'home.html', context)
+
+def sampleHandForAllPlayers():
+    return {"p1" : "5H,3S", "p2" : "JD,TD", "p3" : "AH,AS", "p4" : "KH,QS", "p5" : "2H,7C", "p6" : "5C,5D", "p7" : "7H,7S", "p8" : "9S,4H"}
